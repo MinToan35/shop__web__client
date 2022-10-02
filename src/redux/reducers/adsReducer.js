94ef5cbe697bc63ef63f8b30a6fed2d4bdf665fd
MinToan35 <100203431+MinToan35@users.noreply.github.com>
2022-10-02 11:13:31 +0700

    Update adsReducer.js

--- src/redux/reducers/adsReducer.js
+++ src/redux/reducers/adsReducer.js
@@ -3,7 +3,7 @@ const getAdsReducer = (state = {}, action) => {
     case "GET_ADS_REQUEST":
       return {
         loading: true,
-        postBanners: {
+        postAds: {
           success: false,
           banners: [],
         },
