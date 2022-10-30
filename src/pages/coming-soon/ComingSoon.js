8c34ef3811287bc7e976210af08edb97d57c1595
MinToan35 <mintoan351998@gmail.com>
2022-10-30 13:26:07 +0700

    fix-gif

--- src/pages/coming-soon/ComingSoon.js
+++ src/pages/coming-soon/ComingSoon.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Helmet from "../../components/Helmet";
-
+import "./coming-soon.scss";
 const ComingSoon = () => {
   return (
     <Helmet title="Coming soon">
       <img
         src="https://i.giphy.com/media/anwzK42TsSAd4Ad76M/giphy.webp"
         alt=""
+        className="coming-soon"
       />
     </Helmet>
   );
